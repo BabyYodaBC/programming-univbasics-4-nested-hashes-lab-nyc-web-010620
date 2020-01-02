7b5f8f386863dd836edd287d2f8adb0364ccc5f0
ricky wahba <rjwahba@gmail.com>
2020-01-02 23:21:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,10 +60,12 @@ end
 
 def adding_matz
   
-programmer_hash = {:yukihiro_matsumoto => {
+programmer_hash[:yukihiro_matsumoto] = {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]
- }}
+ }
+  
+}
 
 	programmer_hash = 
  		{
