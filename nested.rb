29992027e64fd13085668a4d79e02cca0b11776c
ricky wahba <rjwahba@gmail.com>
2020-01-02 23:28:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ end
 
 def adding_matz
   
-programmer_hash = {
-  :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]}
-}
-  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -81,6 +75,12 @@ programmer_hash = {
           :languages => ["C"]
         }
      }
+       
+programmer_hash = {
+  :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]}
+}
 
 end
 
